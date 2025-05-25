c82293842b4e6d0990a08ff58a6c15728de90bc7
unknown <zeyadcoding@gmail.com>
2025-05-25 18:07:29 +0300

    most sold product

--- src/index.js
+++ src/index.js
@@ -7,6 +7,11 @@ import "@fortawesome/fontawesome-free/js/all.min";
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
+document.querySelectorAll(".add-to-cart-btn").forEach(item =>{
+    item.addEventListener("click",() =>{
+        alert("تمت الإضافة إلى السلة")
+    })
+});
 console.log("مرحبا بك في متحر عربي")
 
 console.log("hello")
